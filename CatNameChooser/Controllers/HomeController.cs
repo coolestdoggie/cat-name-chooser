bac903b778107b828930935d88cd6cc5f2b7e20c
coolestdoggie <coolestdoggie1@gmail.com>
2024-03-04 13:20:11 +0100

    [ADD]: generate btn behavior

--- CatNameChooser/Controllers/HomeController.cs
+++ CatNameChooser/Controllers/HomeController.cs
@@ -1,12 +1,16 @@
 using CatNameChooser.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace CatNameChooser.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        static HttpClient client = new HttpClient();
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -16,6 +20,23 @@ namespace CatNameChooser.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Generate()
+        {
+            string catName = "";
+            HttpResponseMessage response = client.GetAsync("https://tools.estevecastells.com/api/cats/v1").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                catName = await response.Content.ReadAsStringAsync();
+                char[] charsToTrim = { '[', ']', '"' };
+
+                catName = Regex.Replace(catName, @"[^0-9a-zA-Z\._ ]", string.Empty);
+            }
+
+            TempData["CatName"] = catName;
+
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Privacy()
